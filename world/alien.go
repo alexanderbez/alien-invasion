89c2b765dd40ae2f7d0ec4426913c0b32e622c92
Aleksandr Bezobchuk <abezobchuk@gmail.com>
2018-05-11 14:00:48 -0400

    Implement alien data structure

---
+++ world/alien.go
@@ -0,0 +1,8 @@
+package world
+
+// Alien implements an entity that may occupy a city. It contains a name and
+// the name of the city it currently occupies.
+type Alien struct {
+	name     string
+	cityName string
+}
